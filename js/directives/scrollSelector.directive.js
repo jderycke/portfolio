f65f61adb2400f0098611e4ea5939d760a920ac9
jderycke <jamiederycke@gmail.com>
2016-01-12 20:25:48 +0000

    redesign

--- js/directives/scrollSelector.directive.js
+++ js/directives/scrollSelector.directive.js
@@ -20,8 +20,8 @@
                     angular.element($window).bind('scroll', function () {
                         var currentScroll = this.pageYOffset,
                             currentSection,
-                            id,
-                            scrolled = false;
+                            pageId,
+                            homeHeight = angular.element('#home').height();
 
                         angular.forEach(sections, function (section, index) {
                             var divPosition = angular.element(section).offset().top;
@@ -30,18 +30,16 @@
                                 currentSection = angular.element(section);
                             }
 
-                            id = angular.element(currentSection).prop('id');
+                            pageId = angular.element(currentSection).prop('id');
                             angular.element(document.getElementsByClassName('link')).removeClass('active');
-                            angular.element(document.querySelectorAll('[href="#' + id + '"]')).addClass('active');
-
-                            if (id == 'home' && scrolled) {
-                                angular.element('.site-header').removeClass('scrolled');
-                                scrolled = false;
-                            } else {
-                                angular.element('.site-header').addClass('scrolled');
-                                scrolled = true;
-                            }
+                            angular.element(document.querySelectorAll('[href="#' + pageId + '"]')).addClass('active');
                         });
+
+                        if(currentScroll >= homeHeight) {
+                            angular.element('.scroll.up').addClass('show');
+                        } else {
+                            angular.element('.scroll.up').removeClass('show');
+                        }
                     });
                 }
             };
