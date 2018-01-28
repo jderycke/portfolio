e6b12e89a3025d194d8bcf003817863b03c5d3a8
jamiederycke <jamiederycke@moneysupermarket.com>
2018-01-28 12:56:39 +0000

    Updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="home" class="page page--no-padding">
         <div class="column--flex-wrap">
-            <div class="column column--9-col no-gutters border__right">
+            <div class="column column--10-col no-gutters border__right">
                 <div class="row row__home">
                     <div class="container container__custom">
                         <div class="column--flex-wrap height__full">
@@ -41,26 +41,13 @@
                     </div>
                 </div>
             </div>
-            <div class="column column--3-col no-gutters height__screen">
-                <div class="row height__two-third box__padding">
+            <div class="column column--2-col no-gutters height__screen">
+                <div class="row height__half box__padding">
                     <div class="column">
-                        <h3>Blog roll</h3>
-                        <ul class="table" v-if="items && items.length">
-                            <li class="table__item" v-for="item in items.slice(0, 3)">
-                                <a class="table__link" :href="item.permalink" :title="item.title">
-                                    <span class="table__title" v-html="item.title"></span>
-                                    <small class="table__small">{{item.date | formatDate}}</small>
-                                </a>
-                            </li>
-                        </ul>
-                        <div class="btn__wrapper">
-                            <a href="/blog/" class="btn btn--default">
-                                <span class="btn__text">See more</span>
-                            </a>
-                        </div>
+
                     </div>
                 </div>
-                <div class="row background__image height__third border__top">
+                <div class="row background__image height__half border__top">
                     <div class="column">
                         <div class="display--table height__full">
                             <div class="align--bottom display--table-cell box__padding">
